2f13205db22fbee1b8ecdf2803f5ed569f40693e
HASSANI IDRISSI Rim <74383907+harim12@users.noreply.github.com>
2024-01-22 12:39:05 +0000

    backend conf

---
+++ back/src/main/java/com/rimhassani/confirmerCommande/ConfirmerCommandeApplication.java
@@ -0,0 +1,13 @@
+package com.rimhassani.confirmerCommande;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfirmerCommandeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfirmerCommandeApplication.class, args);
+	}
+
+}
---
+++ back/src/test/java/com/rimhassani/confirmerCommande/ConfirmerCommandeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rimhassani.confirmerCommande;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfirmerCommandeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
